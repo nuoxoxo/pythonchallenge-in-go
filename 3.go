ebc6c625093ec572f3a34e5d947c423e892ef0f7
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-07-27 16:05:04 +0200

    3/linkedlist

---
+++ 3.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+    "fmt"
+    "io/ioutil"
+    "net/http"
+    "regexp"
+)
+
+func main(){
+
+    URL := "http://www.pythonchallenge.com/pc/def/equality.html"
+    response, _ := http.Get(URL)
+    defer response.Body.Close()
+    body, _ := ioutil.ReadAll(response.Body)
+    re := regexp.MustCompile("[^A-Z]+[A-Z]{3}([a-z])[A-Z]{3}[^A-Z]+")
+    matches := re.FindAllStringSubmatch( string(body), -1 )
+    fmt.Println("len/", len(matches))
+    res := ""
+    for i, pair := range matches {
+        res += pair[1]
+        fmt.Println(string(i+'0') + "/", pair[1])
+    }
+    fmt.Println("res/", res)
+}
+
