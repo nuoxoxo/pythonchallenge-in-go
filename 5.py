338728d9d783b87c407f6e687d67895551e53b0f
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-07-27 21:02:10 +0200

    5/channel - edit - del. go code

--- 5.py
+++ 5.py
@@ -1,7 +1,7 @@
 from urllib.request import urlopen
 import pickle
 
-resp = urlopen('http://www.pythonchallenge.com/pc/def/banner.p')
-P = pickle.load(resp)
-print('\n'.join(''.join(k * v for k, v in p) for p in P))
+reader = urlopen('http://www.pythonchallenge.com/pc/def/banner.p').read()
+print(reader)
+print('\n'.join(''.join(k*v for k,v in p) for p in pickle.loads(reader)))
 
