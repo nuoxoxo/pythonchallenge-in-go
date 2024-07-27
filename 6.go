56a24ddca6ab97d1b11f17ef88a72904f8bcd246
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-07-27 23:43:46 +0200

    6/hockey

---
+++ 6.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+    "fmt"
+    "regexp"
+    "archive/zip"
+    "io/ioutil"
+)
+
+func main(){
+
+    // zip reader
+    openreader, err := zip.OpenReader("./channel.zip")
+    if err != nil {
+        fmt.Println("open reader/", err)
+    }
+    defer openreader.Close()
+
+    // zip files - single file reader
+    // begin w/ 90052
+    re := regexp.MustCompile(`nothing is (\d+)`)
+    comments := []string{}
+    s := "90052"
+    e := ".txt"
+    for {
+        // get access to a file in zip
+        fname := s + e
+        f, err := read_inside_zip(openreader, fname)
+        if err != nil {
+            fmt.Println("read inside zip/", fname, err)
+        }
+        // open the target file
+        fopen, err := f.Open()
+        if err != nil {
+            fmt.Println("f.open()/", fname, err)
+        }
+        // get its content & comment
+        cont, err := ioutil.ReadAll(fopen)
+        cmt := f.Comment
+        char := cmt
+        if char == "\n" {
+            char = "( newline )"
+        }
+        fmt.Println(s, "\t", string(cont), char)
+        comments = append(comments, cmt)
+        // get the next filename
+        match := re.FindSubmatch(cont)
+        if match == nil {
+            break
+        }
+        //fmt.Println("match[0]/", string(match[0]))
+        //fmt.Println("match[1]/", string(match[1]))
+        s = string(match[1])
+    }
+    res := ""
+    for _, cmt := range comments {
+        res += cmt
+    }
+    fmt.Println(res)
+}
+
+// unzip
+
+func read_inside_zip (openreader * zip.ReadCloser, target string) (*zip.File, error) {
+
+    for _, f := range openreader.File {
+        if f.Name != target {
+            continue
+        }
+        return f, nil
+    }
+    return nil, fmt.Errorf("err/not found: %s", target)
+}
+
+
