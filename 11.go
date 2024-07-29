36c4eb3fcac6d25f9317dbcd99f3735df873573b
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-07-29 21:31:01 +0200

    11/evi

---
+++ 11.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+    _"fmt"
+    "os"
+    "image"
+    "image/jpeg"
+)
+
+func main() {
+
+    imgfile, _ := os.Open("cave.jpg")
+    defer imgfile.Close()
+
+    img, _, _ := image.Decode( imgfile )
+    bounds := img.Bounds()
+    Even := image.NewRGBA( bounds )
+    Odd := image.NewRGBA( bounds )
+    W := bounds.Dx()
+    H := bounds.Dy()
+    var r, c int
+    r = 0
+    for r < H {
+        c = 0
+        for c < W {
+            pix := img.At(c, r)
+            rc := r + c
+            if rc % 2 == 0 {
+                Even.Set( c, r, pix )
+            } else {
+                Odd.Set( c, r, pix )
+            }
+            c++
+        }
+        r++
+    }
+
+    // save both jpg
+    imgfileEven, _ := os.Create("1.jpg")
+    defer imgfileEven.Close()
+    jpeg.Encode( imgfileEven, Even, nil )
+
+    imgfileOdd, _ := os.Create("2.jpg")
+    defer imgfileOdd.Close()
+    jpeg.Encode (imgfileOdd, Odd, nil)
+}
+
