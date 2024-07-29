b725c2a14a042244050c834413f4683784e69063
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-07-30 00:02:01 +0200

    12/disppt - print typeof

--- 12.go
+++ 12.go
@@ -1,15 +1,16 @@
 package main
 
 import (
-    _"fmt"
+    "fmt"
+    "reflect"
     "os"
-    _"io"
     "io/ioutil"
     "strconv"
 )
 
 func main(){
     content, _ := ioutil.ReadFile("files/evil2.gfx")
+    fmt.Println(content[:42], reflect.TypeOf(content) ) // uint8
     N := len(content)
     i := 0
     for i < 5 {
@@ -20,8 +21,8 @@ func main(){
             outfile.Write( content[j : j + 1] )
             j += 5
         }
-        i++
         outfile.Close()
+        i++
     }
 }
 
