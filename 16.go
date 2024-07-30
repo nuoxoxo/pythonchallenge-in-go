68d9777ff482f91d7a4a0ffd1ab0d3ab3a03cfa1
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-07-31 01:00:59 +0200

    16/still trying

--- 16.go
+++ 16.go
@@ -4,10 +4,40 @@ import (
     "fmt"
     "net/http"
     "io/ioutil"
+    "image"
+    "image/gif"
+    "os"
+    "reflect"
 )
 
 func main(){
-    
+    mozart, _ := os.Open("files/mozart.gif")
+    defer mozart.Close()
+    mozartgif, _ := gif.Decode(mozart)
+    bounds := mozartgif.Bounds()
+    fmt.Println("bounds/", bounds, reflect.TypeOf(bounds))
+    fmt.Println("rows", bounds.Dy())
+    fmt.Println("cols", bounds.Dx())
+    R := bounds.Dy()
+    C := bounds.Dx()
+
+    // paletted := image.NewPaletted(bounds, mozartgif.(*image.Paletted).Palette)
+    pimg, _ := mozartgif.(*image.Paletted)
+    r := 0
+    for r < R {
+        row := make(map[uint8]int)
+        c := 0
+        for c < C {
+            color := pimg.ColorIndexAt(c, r)
+            row[color]++
+            c++
+        }
+        for k, v := range row {
+            fmt.Println("row/", k, v)
+        }
+        fmt.Println("")
+        r++
+    }
 }
 
 func init(){
