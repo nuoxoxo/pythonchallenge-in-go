edfc7e0a6a696b3e63c0e2774462ffff124a3604
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-07-30 14:53:45 +0200

    14/bugfixing

--- 14.go
+++ 14.go
@@ -4,17 +4,45 @@ import (
     "fmt"
     "io/ioutil"
     "net/http"
+    "image"
+    "image/jpeg"
+    "image/png"
+    _"image/color"
+    "os"
 )
 
 var PAGE string
 
 func main(){
-    fmt.Println(PAGE, "\nbody ends/")
+    fmt.Println(PAGE, "\nbody ends/\n\n")
+
+    // let's draw
+    raw, _ := os.Open("files/wire.png")
+    defer raw.Close()
+    var wire image.Image 
+    wire, _ = png.Decode(raw)
+    res := image.NewRGBA(image.Rect(0,0,101,101))
+    D := [][]int{{0,1},{1,0},{0,-1},{-1,0}}
+    i := 0
+    offset := 100
+    c, r := 0, 0
+    for i < 100 {
+        color := wire.At(i, 0)//.RGBA()
+         
+        for _, d := range D {
+            r, c = r + d[0] * offset, c + d[1] * offset
+            res.Set(c, r, color)
+            fmt.Println(i, c, r)
+            i += 1
+            offset -= 1
+        }
+    }
+    f, _ := os.Create("files/res.jpg")
+    defer f.Close()
+    jpeg.Encode(f, res, nil)
 }
 
-
 func init(){
-
     URL := "http://www.pythonchallenge.com/pc/return/italy.html"
     u, p := "huge","file"
 
@@ -29,4 +57,5 @@ func init(){
     body, _ := ioutil.ReadAll(resp.Body)
     PAGE = string(body)
 }
+
 
