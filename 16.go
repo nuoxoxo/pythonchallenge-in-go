42319b212bf22da67faef57516838e09b0c9b988
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-07-31 16:06:55 +0200

    16/longest substr - attempting

--- 16.go
+++ 16.go
@@ -9,19 +9,21 @@ import (
     "image/color"
     "os"
     "reflect"
+    "strconv"
 )
 
 func main(){
-    FILE, _ := os.Open("files/mozart.gif")
-    defer FILE.Close()
-    mozart, _ := gif.Decode(FILE)
+    gf, _ := os.Open("files/mozart.gif")
+    defer gf.Close()
+    mozart, _ := gif.Decode(gf)
     bounds := mozart.Bounds()
     var R, C, r, c int
     R, C = bounds.Dy(), bounds.Dx()
     fmt.Println("size/", bounds, reflect.TypeOf(bounds))
     fmt.Println("rows/", R, "cols/", C)
     res := image.NewPaletted( bounds, nil )
-    fmt.Println("init/", res, reflect.TypeOf(res))
+    fmt.Println("init/", /*res,*/ reflect.TypeOf(res))
+    
     r = 0
     for r < R {
         // row reprented as a rgb(a) slice
@@ -29,14 +31,38 @@ func main(){
         c = 0
         for c < C {
             cl := mozart.At(c, r)
-            fmt.Println("color/", cl, reflect.TypeOf(cl))
+            //fmt.Println("color/", cl, reflect.TypeOf(cl))
             row[c] = cl
+            c++
         }
         // here we do longest uni-char substring 
-        // todo...
+        // todo... doing...
+        var commoncolor color.Color = row[0] // most seen
+        var currentcolor color.Color = commoncolor // now-inspecting
+        scurr := 0 // curr s(tart)
+        s, e, maxlen := 0, 0, 0
+        c = 1
+        for c < C {
+            if currentcolor != row[c] {
+                dist := c - scurr
+                if maxlen < dist {
+                    commoncolor = row[c]
+                    s = scurr
+                    e = c
+                    maxlen = dist
+                }
+                currentcolor = row[c]
+                scurr = c
+            }
+            c++
+        }
+        se := strconv.Itoa(s) + "-" + strconv.Itoa(e)
+        fmt.Println(r, "\b/", "color/", commoncolor, se, "len/", maxlen)
         r++
+        // obsevation/
+        //  - lots of len-5 segments, not seem to be pink
+        //  - some rows dont have any len-5 segments, surprise
     }
-
 }
 
 func init(){
