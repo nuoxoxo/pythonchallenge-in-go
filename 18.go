f35e2c1360b938149d0d6ca081f0f3a3adb83923
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-08-02 01:45:55 +0200

    18/no way to get common

--- 18.go
+++ 18.go
@@ -1,46 +1,53 @@
 package main
 
 import (
+    "fmt"
+    "os"
+    "compress/gzip"
     _"strconv"
     "bytes"
-    "compress/gzip"
     "encoding/hex"
-    "fmt"
-    _"io"
-    "os"
     "strings"
+    "reflect"
 
     "github.com/pmezard/go-difflib/difflib"
 )
 
+var buffer *bytes.Buffer
+
 func main() {
 
-    file, _ := os.Open("files/deltas.gz")
-    defer file.Close()
-
-    gzReader, _ := gzip.NewReader(file)
-    defer gzReader.Close()
+    //  observation/ 3 slots of 32/sp in the middle
+    //      len/108 ...
+    //      52 / 48 uint8
+    //      53 / 32 uint8
+    //      54 / 32 uint8
+    //      55 / 32 uint8
+    //      56 / 56 uint8
 
     var L, R []string
-    buffer := new(bytes.Buffer)
-    _, _ = buffer.ReadFrom(gzReader)
-
     lines := strings.Split(buffer.String(), "\n")
     for _, line := range lines {
         if len(line) < 56 {
-            fmt.Println("56 ?/", len(line))
+            fmt.Println("empty?/", len(line))
+            //L = append(L, line)
+            //continue
         } else {
             L = append(L, line[:53])
             R = append(R, line[56:])
         }
     }
 
-    diff, _ := difflib.GetUnifiedDiffString(difflib.UnifiedDiff{
-        A: difflib.SplitLines(strings.Join(L, "\n")),
-        B: difflib.SplitLines(strings.Join(R, "\n")),
+    diff, _ := difflib.GetUnifiedDiffString( difflib.UnifiedDiff {
+        A:  difflib.SplitLines(strings.Join(L, "\n")),
+        B:  difflib.SplitLines(strings.Join(R, "\n")),
         Context: 3,
     })
 
+
+    fmt.Println(diff, "\nend/")
+
+    // create images
     p1, _ := os.Create("p1.png")
     defer p1.Close()
 
@@ -50,46 +57,50 @@ func main() {
     p3, _ := os.Create("p3.png")
     defer p3.Close()
 
-    //fmt.Println(diff)
-
     for i, line := range strings.Split(diff, "\n") {
         if len(line) == 0 {
+            fmt.Println("line/", i, "(null)")
             continue
         }
 
-        var bs []byte
+        bytes := []byte{}
+
         if !strings.HasPrefix(line, "@") {//&& (strings.HasPrefix(line, "+") || strings.HasPrefix(line, "-")) {
             data := line[1:]
-            fmt.Println(i, line)
-            bs, _ = hexstring2bytes(data)
+            // fmt.Println(i, line)
             //data := strings.TrimSpace(line[1:])
-            //bs, _ = hexstring2bytes(data)
+            bytes, _ = hexpair(data)
         }
-        //if len(bs) < 1 { continue } 
-        //switch {
-        if strings.HasPrefix(line, "+") {
-            p2.Write(bs)
-            //fmt.Println("+/", line)
-        } else if strings.HasPrefix(line, "-") {
-            p3.Write(bs)
-            //fmt.Println("-/", strconv.Quote(line))
-        } else if strings.HasPrefix(line, " "){
-            p1.Write(bs)
-            fmt.Println(i, "prefix!=@/", len(bs), line)
-        } 
-        /*else {
+        if strings.HasPrefix(line, "+") { p1.Write(bytes) }
+        if strings.HasPrefix(line, "-") { p2.Write(bytes) }
+        if strings.HasPrefix(line, "?") { p3.Write(bytes) }
+        if !strings.HasPrefix(line, "+") && !strings.HasPrefix(line, "-") && !strings.HasPrefix(line, " "){
             fmt.Println(i, "default/", line)
-        }*/
-        //default:
-        //fmt.Println(i, "default/", line)
         }
     }
-
-func hexstring2bytes(hexstring string) ([]byte, error) {
-    hexstring = strings.ReplaceAll(hexstring, " ", "")
-    if len(hexstring) % 2 != 0 {
-        hexstring = "0" + hexstring
-    }
-    return hex.DecodeString(hexstring)
 }
+
+func hexpair(hexstring string) ([]byte, error) {
+
+    res := strings.ReplaceAll(hexstring, " ", "")
+    if len(res) % 2 != 0 {
+        res = "0" + res
+    }
+    return hex.DecodeString(res)
+}
+
+func init() {
+
+    file, _ := os.Open("files/deltas.gz")
+    defer file.Close()
+
+    gzReader, _ := gzip.NewReader(file)
+    defer gzReader.Close()
+
+    buffer = new(bytes.Buffer)
+    _, _ = buffer.ReadFrom(gzReader)
+
+    fmt.Println(reflect.TypeOf( buffer ))
+}
+
 
