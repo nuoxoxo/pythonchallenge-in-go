c35bc0f064a9dd6bd3b93d7bb391fe7aa640e063
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-08-06 12:28:43 +0200

    12/done

--- 12.go
+++ 12.go
@@ -4,18 +4,39 @@ import (
     "fmt"
     "reflect"
     "os"
+    "io"
     "io/ioutil"
     "strconv"
     "net/http"
 )
 
 func main(){
-    content, _ := ioutil.ReadFile("files/evil2.gfx")
+
+    Filename := "evil2.gfx"
+
+    f, err := os.Create( Filename )
+    defer f.Close()
+
+    URL := "http://www.pythonchallenge.com/pc/return/" + Filename
+    ups, mid := "hugefile", 4
+    user, pass := ups[:mid], ups[mid:]
+    conn := &http.Client{}
+    req, err := http.NewRequest("GET", URL, nil)
+    fmt.Println("err/GET", err)
+
+    req.SetBasicAuth( user, pass )
+    resp, err := conn.Do( req )
+    fmt.Println("err/conn.DO", err, resp.StatusCode)
+    defer resp.Body.Close()
+
+    _, _ = io.Copy(f, resp.Body)
+
+    content, _ := ioutil.ReadFile( Filename )
     fmt.Println(content[:42], reflect.TypeOf(content) ) // uint8
     N := len(content)
     i := 0
     for i < 5 {
-        name := "files/0" + strconv.Itoa(i) + ".jpg"
+        name := "12_" + strconv.Itoa(i) + ".jpg"
         outfile, _ := os.Create( name )
         j := i
         for j < N {
@@ -27,14 +48,12 @@ func main(){
     }
 
     // extra : get a msg for next level
-    URL := "http://www.pythonchallenge.com/pc/return/evil4.jpg"
-    user := "huge"
-    pass := "file"
-    conn := & http.Client{}
-    req, err := http.NewRequest("GET", URL, nil)
+    URL = "http://www.pythonchallenge.com/pc/return/evil4.jpg"
+    conn = & http.Client{}
+    req, err = http.NewRequest("GET", URL, nil)
     if err != nil {fmt.Println("err/", err)}
     req.SetBasicAuth( user, pass )
-    resp, _ := conn.Do(req)
+    resp, _ = conn.Do( req )
     defer resp.Body.Close()
     body, _ := ioutil.ReadAll(resp.Body)
     fmt.Println("msg/", string(body))
