3deb782aa8e154d4638eca311a0cfa01c0aec08c
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-08-09 00:27:19 +0200

    24/init - got the data

--- 24.go
+++ 24.go
@@ -4,26 +4,62 @@ import (
     "fmt"
     "net/http"
     "io/ioutil"
+    "regexp"
+    "reflect"
+    "bytes"
+    "image"
+    "image/png"
 )
 
 const URL string = "http://www.pythonchallenge.com/pc/hex/"
-//const Yell, Cyan, Rest string = "\033[33m", "\033[36m", "\033[0m"
+const Yell, Cyan, Rest string = "\033[33m", "\033[36m", "\033[0m"
 
 func main(){}
 
-func init(){
+func init() {
 
-    // GET
+    // get url
     conn := & http.Client{}
     req, _ := http.NewRequest("GET", URL + "ambiguity.html", nil)
     req.SetBasicAuth( "butter", "fly" )
     resp, _ := conn.Do(req)
     defer resp.Body.Close()
-
-    temp, _ := ioutil.ReadAll(resp.Body)
-    body := string(temp)
-
+    data, _ := ioutil.ReadAll(resp.Body)
+    body := string(data)
     fmt.Println(body, "\nbody ends/")
+    // get png
+    re := regexp.MustCompile(`(?s)<img src="(.*?)"`)
+    sub := re.FindAllStringSubmatch(body, -1)[0][1]
+    conn = & http.Client{}
+    req, _ = http.NewRequest("GET", URL + sub, nil)
+    req.SetBasicAuth( "butter", "fly" )
+    resp, _ = conn.Do(req)
+    defer resp.Body.Close()
+    // read png
+    data, _ = ioutil.ReadAll(resp.Body)
+    reader := bytes.NewReader(data)
+    fmt.Println("typ/data", reflect.TypeOf(data), "typ/reader", reflect.TypeOf(reader))
+    img, _ := png.Decode(reader)
+    bounds := img.Bounds()
+    fmt.Println("typ/png", reflect.TypeOf(img), "bounds/", bounds)
+    //if tmp, ok := img.(*image.NRGBA); ok { inspect_img(tmp) } // INSPECT
 }
+
+func inspect_img(img *image.NRGBA) {
+    bounds := img.Bounds()
+    X, Y := bounds.Max.X, bounds.Max.Y
+    var x, y int
+    y = 0
+    for y < Y {
+        x = 0
+        for x < X {
+            r, g, b, _ := img.At(x, y).RGBA()
+            fmt.Println("x,y/", x, y, "rgb", r / 257, g / 257, b / 257)
+            x++
+        }
+        y++
+    }
+}
+
 
 
