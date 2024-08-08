af004eaa4785e0a1af4b90427f6a9d18268a8e1e
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-08-08 11:49:12 +0200

    22/center-dotted frames total to 5

--- 22.go
+++ 22.go
@@ -5,18 +5,61 @@ import (
     "net/http"
     "io/ioutil"
     "regexp"
-    _"strings"
-    _"strconv"
-    _"os"
-    _"reflect"
+    "reflect"
+    "bytes"
+    "image"
+    "image/gif"
+    "image/color"
 )
 
+var rawgif []byte
 const URL string = "http://www.pythonchallenge.com/pc/hex/"
-const Yell string = "\033[33m" 
-const Cyan string = "\033[36m" 
-const Rest string = "\033[0m"
+const Yell, Cyan, Rest string = "\033[33m", "\033[36m", "\033[0m"
 
-func main(){}
+func main(){
+
+    fmt.Println("typ/", reflect.TypeOf(rawgif), "len/", len(rawgif))
+
+    // treat/read data as gif
+    reader := bytes.NewReader(rawgif)
+    readgif, _ := gif.DecodeAll(reader)
+    fmt.Println("typ/reader", reflect.TypeOf(reader))
+    fmt.Println("typ/readgif", reflect.TypeOf(readgif))
+
+    // count frames, all frames have same bounds
+    nframes := 0
+    frameset := make(map[image.Rectangle]bool)
+
+    pxlmap := make(map[color.Color]int)
+    ncenter := 0
+    for _, frame := range readgif.Image {
+        nframes++
+        frameset[frame.Bounds()] = true
+
+        // inspect all pixels
+        X, Y := frame.Bounds().Max.X, frame.Bounds().Max.Y
+        x, y := 0, 0
+        for y < Y {
+            x = 0
+            for x < X {
+                r, g, b, _ := frame.At(x, y).RGBA()
+                if r / 257 == 8 && g / 257 == 8 && b / 257 == 8 {
+                    fmt.Println(Cyan + "color 8/" + Rest, x, y)
+                    if x == 100 && y == 100 { ncenter++ }
+                }
+                pxlmap[frame.At(x, y)]++
+                x++
+            }
+            y++
+        }
+    }
+    fmt.Println("nframes/", nframes, "set/", frameset)
+    fmt.Println(Yell + "\t^ a total of 133 frames" + Rest)
+    for k, v := range pxlmap { fmt.Println("color/", k, "qty", v) }
+    fmt.Println(Yell + "\t^ one of 2 existing colors has but 1-pix per frame" + Rest)
+    fmt.Println("ncenter/", "coor(100, 100) reached", ncenter, "times")
+
+}
 
 func init(){
 
@@ -30,18 +73,19 @@ func init(){
     // to be told how url should be modified
     temp, _ := ioutil.ReadAll(resp.Body)
     body := string(temp)
+
+    // get sub2, ie. white.gif
     re := regexp.MustCompile(`(?s)maybe (.*?) would`)
     matches := re.FindAllStringSubmatch(body, -1)
-    sub2 := matches[0][1] // white.gif
+    sub2 := matches[0][1]
 
     conn = & http.Client{}
     req, _ = http.NewRequest("GET", URL + sub2, nil)
     req.SetBasicAuth( "butter", "fly" )
     resp, _ = conn.Do(req)
     defer resp.Body.Close()
-    temp, _ = ioutil.ReadAll(resp.Body)
-    body = string(temp)
-    fmt.Println(body[:42 * 42], "\nbody ends/")
+
+    rawgif, _ = ioutil.ReadAll(resp.Body)
 }
 
 
