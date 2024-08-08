43e5bb3455bf44df02ae32aa837438a5b118d9ad
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-08-08 13:02:22 +0200

    22/incorrect output but idea is RIGHT

--- 22.go
+++ 22.go
@@ -32,6 +32,13 @@ func main(){
 
     pxlmap := make(map[color.Color]int)
     ncenter := 0
+    xoffset, yoffset, offset := 1000, 1000, 98 // now we know 98,98
+    xbound, ybound := -1, -1
+
+    // we also know 100,100 happens 5 times
+    FiveGuys := [][][]int{}
+    for i := 0; i < 5; i++ { FiveGuys = append(FiveGuys, [][]int{}) }
+
     for _, frame := range readgif.Image {
         nframes++
         frameset[frame.Bounds()] = true
@@ -46,6 +53,13 @@ func main(){
                 if r / 257 == 8 && g / 257 == 8 && b / 257 == 8 {
                     fmt.Println(Cyan + "color 8/" + Rest, x, y)
                     if x == 100 && y == 100 { ncenter++ }
+                    if xoffset > x { xoffset = x } // know a priori:
+                    if yoffset > y { yoffset = y } //   offset/ 98,98
+                    if xbound < x { xbound = x }
+                    if ybound < y { ybound = y }
+
+                    FiveGuys[ncenter-1] = append(
+                        FiveGuys[ncenter-1], []int{x - offset, y - offset})
                 }
                 pxlmap[frame.At(x, y)]++
                 x++
@@ -58,7 +72,40 @@ func main(){
     for k, v := range pxlmap { fmt.Println("color/", k, "qty", v) }
     fmt.Println(Yell + "\t^ one of 2 existing colors has but 1-pix per frame" + Rest)
     fmt.Println("ncenter/", "coor(100, 100) reached", ncenter, "times")
+    fmt.Println("offsets/", xoffset, yoffset)
+    fmt.Println("rbounds/", xbound, ybound)
 
+    // Bruteforce
+    xbound, ybound = xbound - xoffset + 1, ybound - yoffset + 1
+    for _, coors := range FiveGuys {
+        char := [][]string{}
+        r := 0
+        for r < ybound {
+            temp := []string{}
+            c := 0
+            for c < xbound {
+                temp = append(temp, " ")
+                c++
+            }
+            r++
+            char = append(char, temp)
+        }
+        for _, coor := range coors {
+            x, y := coor[0], coor[1]
+            char[x][y] = "@"
+        }
+        r = 0
+        for r < ybound {
+            c := 0
+            for c < xbound {
+                fmt.Print(char[r][c])
+                c++
+            }
+            fmt.Println()
+            r++
+        }
+        fmt.Println("---")
+    }
 }
 
 func init(){
