a52cd491d8a169ded9544829b6d45d6b3bc006b8
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-08-11 11:20:03 +0200

    24/26 done w/ correction

--- 26.go
+++ 26.go
@@ -6,10 +6,14 @@ import (
     "os/exec"
     "strings"
     "sync"
+    "io"
+    "crypto/md5"
+    "encoding/hex"
 )
 
-// md5/ bbb8b499a0eef99b52c7f13f4e78c24b
+const testmd5 string = "bbb8b499a0eef99b52c7f13f4e78c24b"
 const yelo, cyan, rest string = "\033[33m", "\033[36m", "\033[0m"
+func yell(s string) string {return yelo + s + rest}
 
 func main(){
 
@@ -30,7 +34,7 @@ func main(){
         cmd = exec.Command(bingo, "run", "24.go")
         cmd.Stdout, cmd.Stderr = os.Stdout, os.Stderr
         err = cmd.Run()
-        if err != nil { fmt.Println(yelo+"err/24"+rest, err) } // no panic
+        if err != nil { fmt.Println(yell("err/24"), err) } // no panic
     }()
     wg.Wait()
 
@@ -38,20 +42,56 @@ func main(){
     cmd := exec.Command("unzip", "out.zip")
     cmd.Stdout, cmd.Stderr = os.Stdout, os.Stderr
     err := cmd.Run()
-    if err != nil { fmt.Println(yelo+"err/out"+rest, err) }
+    if err != nil { fmt.Println(yell("err/out"), err) }
+
+    
+    // bruteforce correction
+    zipfile, err := os.Open("mybroken.zip")
+    fmt.Println(yell("err/zip"), err)
+    defer zipfile.Close()
+    stat, err := zipfile.Stat()
+    fmt.Println(yell("err/stat"), err)
+    data := make([]byte, stat.Size())
+    _, err = io.ReadFull(zipfile, data)
+    fmt.Println(yell("err/read"), err)
+    found := false
+    for i, b := range data {
+        repl := 0
+        for repl < 256 {
+            //modified := data[:i] + uint8(repl) + data[i + 1:]
+            data[i] = uint8(repl)
+            attempt := md5.Sum(data)
+            newmd5 := hex.EncodeToString(attempt[:])
+            if newmd5 == testmd5 {
+                _ = os.WriteFile("fixed.zip", data, 0644)
+                fmt.Println("i/bugbyte", i, b, "fix/", uint8(repl))
+                found = true
+            }
+            repl++
+        }
+        if found { break }
+        data[i] = b
+    }
 
     // unzip mybroken.zip
+    //  broken but the image still shows
+    /*
     cmd = exec.Command("unzip", "mybroken.zip")
     cmd.Stdout, cmd.Stderr = os.Stdout, os.Stderr
     err = cmd.Run()
-    if err != nil { fmt.Println(yelo+"err/mybroken"+rest, err) }
+    if err != nil { fmt.Println(yell("err/mybroken"), err) }
+    */
+
+    // unzip fixed.zip
+    cmd = exec.Command("unzip", "fixed.zip")
+    cmd.Stdout, cmd.Stderr = os.Stdout, os.Stderr
+    err = cmd.Run()
+    if err != nil { fmt.Println(yell("err/fixed"), err) }
 
     // rmv zip
     cmd = exec.Command("rm", "maze.jpg", "out.zip", "mybroken.zip")
     cmd.Stdout, cmd.Stderr = os.Stdout, os.Stderr
     err = cmd.Run()
-    if err != nil { fmt.Println(yelo+"err/rm"+rest, err) }
-
-    // correction/ TODO
+    if err != nil { fmt.Println(yell("err/rm"), err) }
 }
 
