6613f1eda0b5cfb77d316730ee3d1a2b8721cd8e
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-09-06 23:24:10 +0200

    31/finally!

--- 31.go
+++ 31.go
@@ -10,7 +10,7 @@ import (
     "image"
     "image/color"
     "image/gif"
-    _"image/png"
+    "image/png"
     "os"
     "bytes"
     _"io"
@@ -23,12 +23,15 @@ func main(){
 }
 
 func init(){
+
     // main page
+
     data, _ := getbody("ring/grandpa.html", "repeat", "switch")
     fmt.Println(string(data))
     yell("body ends/\n")
 
     // href
+
     re := regexp.MustCompile(`(?s)<a href="../(.*?)"`)
     sub := re.FindAllStringSubmatch(string(data), -1)[0][1]
     fmt.Println(Cyan + "sub/" + Rest, sub)
@@ -41,6 +44,7 @@ func init(){
     matches := re.FindAllStringSubmatch(string(data), -1)
 
     // get the 4 floats
+
     var fourfloats [4]float64
     for i, m := range matches[0][1:] {
         val, _ := strconv.ParseFloat(string(m), 64)
@@ -49,6 +53,7 @@ func init(){
     fmt.Println(Cyan + "fourfloats/" + Rest, fourfloats)
 
     // get/original mandelbrot.GIF on main page
+
     prev := sub[:5]
     re = regexp.MustCompile(`(?s)img src="(.*?)"`)
     sub = re.FindAllStringSubmatch(string(data), -1)[0][1]
@@ -61,6 +66,7 @@ func init(){
     fmt.Println(Yell + "type/data " + Rest, reflect.TypeOf(data))
 
     // read original mandelbrot.GIF on main page
+
     bytereader := bytes.NewReader(data)
     img, err := gif.Decode( bytereader )
     if err != nil { fmt.Println("gif.Decode/err", err) }
@@ -83,11 +89,12 @@ func init(){
 
 
     // TODO - next step is to create the new GIF w/ the given floats and maxIter
-
     // Step/ make new mandelbrot
+
     var L,T,X,Y float64 = fourfloats[0],fourfloats[1],fourfloats[2],fourfloats[3]
 
     // define a greyscale palette with 256 levels of grey
+
     var greypalette color.Palette
     for i := 0; i < 256; i++ {
         greypalette = append(greypalette, color.Gray{ Y: uint8(i) })
@@ -95,10 +102,12 @@ func init(){
     newmandelbrot := image.NewPaletted(image.Rect(0, 0, W, H), greypalette)
 
     // Generate the Mandelbrot fractal
+
     for h := 0; h < H; h++ {
+    //for h := H - 1; h > -1; h-- {
         for w := 0; w < W; w++ {
-            realpt := L + float64(w)*X/float64(W)
-            imagpt := T + float64(h)*Y/float64(H)
+            realpt := L + float64(w) * (X / float64(W))
+            imagpt := T + float64(h) * (Y / float64(H))
             c := complex(realpt, imagpt)
             z := complex(0, 0)
             var i int
@@ -109,9 +118,26 @@ func init(){
                 }
             }
 
-            grey := uint8(255 * i / 128)
+            // BUG - this part
 
+            if i == 128 { i-- }
+            var grey uint8
+            grey = uint8(i)// % 128)
             newmandelbrot.SetColorIndex(w, H-1-h, grey)
+
+            /*
+            var grey uint8
+            if 0 < i && i < 127 {
+                grey = uint8(i)
+                newmandelbrot.SetColorIndex(w, H-1-h, grey)
+            } else {
+                grey = uint8(255 * (i / 128))
+            }
+            */
+            //grey := uint8(255 * (i / 128))
+
+            //newmandelbrot.SetColorIndex(w, h, grey)
+            //newmandelbrot.SetColorIndex(w, H-1-h, grey)
         }
     }
 
@@ -122,6 +148,7 @@ func init(){
     if err != nil { panic(err) }
 
     // reading pal2
+
     pal2 := newmandelbrot.Palette
     //bounds := imgpal.Bounds()
     //W, H := bounds.Max.X, bounds.Max.Y
@@ -129,7 +156,8 @@ func init(){
     flag = false
     for y := 0; y < H; y++ {
         for x := 0; x < W; x++ {
-            idx := newmandelbrot.ColorIndexAt(x, y)
+            idx := newmandelbrot.ColorIndexAt(x, H-y-1) // trying ...
+            //idx := newmandelbrot.ColorIndexAt(x, y)
             col := pal2[idx]
             fmt.Println("idx2/", idx, "col/", col)
             if x == 42 {flag = true} // to be modif. - TODO
@@ -137,9 +165,56 @@ func init(){
         if flag {break}
     }
 
+    diffs := [][]uint8{}
+    for y := 0; y < H; y++ {
+        for x := 0; x < W; x++ {
+            a := imgpal.ColorIndexAt(x, y)
+            b := newmandelbrot.ColorIndexAt(x, y)
+            if a != b && b != 128 {
+                diffs = append(diffs, []uint8{a, b})
+            }
+        }
+    }
+    fmt.Println("len/", len(diffs), "- assert/1679")
+    fmt.Println(":21/", diffs[:21])
+
+    N := len(diffs)
+    // finding out factors, assert ---> should be 2 values
+    factors := []int{}
+    for N > 1 {
+        fac := 2
+        found := false
+        for fac < N / 2 + 1 {
+            if N % fac == 0 {
+                found = true
+                factors = append(factors, fac)
+                N /= fac
+                break
+            }
+            fac++
+        }
+        ///*
+        if ! found {
+            factors = append(factors, N)
+            break
+        }
+        //*/
+    }
+    fmt.Println(factors)
+    newW, newH := factors[0], factors[1]
+    finaldata := image.NewGray(image.Rect(0, 0, newW, newH))
+    for i, pair := range diffs {
+        a, b := pair[0], pair[1]
+        pix := 255
+        if a > b {
+            pix = 0
+        }
+        finaldata.SetGray(i % newW, i / newW, color.Gray{ Y: uint8(pix) })
+    }
+    finalfile, _ := os.Create("what.png")
+    defer finalfile.Close()
+    png.Encode(finalfile, finaldata)
 }
-
-
 
 //
 
