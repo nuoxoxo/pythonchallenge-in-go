786a90b5ccceeb8288a3c775b2a7c2440e3a43fc
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-09-17 21:46:13 +0200

    3/linkedlist

--- 3.go
+++ 3.go
@@ -8,18 +8,16 @@ import (
 )
 
 func main(){
-
     URL := "http://www.pythonchallenge.com/pc/def/equality.html"
     response, _ := http.Get(URL)
     defer response.Body.Close()
     body, _ := ioutil.ReadAll(response.Body)
     re := regexp.MustCompile("[^A-Z]+[A-Z]{3}([a-z])[A-Z]{3}[^A-Z]+")
     matches := re.FindAllStringSubmatch( string(body), -1 )
-    fmt.Println("len/", len(matches))
     res := ""
     for i, pair := range matches {
         res += pair[1]
-        fmt.Println(string(i+'0') + "/", pair[1])
+        fmt.Println(string(i + '0') + "/", pair[1])
     }
     fmt.Println("res/", res)
 }
